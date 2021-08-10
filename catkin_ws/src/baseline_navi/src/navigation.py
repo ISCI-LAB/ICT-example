f225592666e10e09a8c7806f5ddeafb1fbfee907
H-HChen <annhunter201@gmail.com>
2021-08-10 18:12:35 +0800

    Modify coding style

--- catkin_ws/src/baseline_navi/src/navigation.py
+++ catkin_ws/src/baseline_navi/src/navigation.py
@@ -12,67 +12,54 @@ sys.path.append('~/low_cost_ws/src/pyrobot/examples/grasping')
 from locobot import Grasper
 
 last_msg = ''
-class Robott(object): 
+class LocobotController(object): 
     def __init__(self):
-        self.stage=0
+        self.stage = 0
         self.bot = Robot(
-        "locobot",
-        base_config={
-            "base_controller": "ilqr",
-            "base_planner": "none",
-        },
+            "locobot",
+            base_config={
+                "base_controller": "ilqr",
+                "base_planner": "none",
+            }
         )
         self.goal = rospy.Subscriber('goal', PoseStamped, self.goal_cb, queue_size=1)
         self.stagecb = rospy.Subscriber('stage',UInt8,self.stage_cb,queue_size=1)
-    # def navigate(self):
-        
-    #     rospy.loginfo("Wait for inputting goal points.")
-        
-        
-    def stage_cb(self,stage_msg):
-        print("fuck",stage_msg)
-        tmp=stage_msg.data
-        if tmp==2:
-            self.stage=2
-            rospy.loginfo("stage => 2. goto apriltag")
-        # rospy.loginfo("Achieve the goal.")
-        
-    def goal_cb(self,goal_msg):
-        global last_msg
-        
 
-    # stage = rospy.get_param('/goal_point/stage')
-    # stage = int(stage)
+    def stage_cb(self, stage_msg):
+        # print(stage_msg)
+        tmp = stage_msg.data
+
+        if tmp == 2:
+            self.stage = 2
+            rospy.loginfo("stage => 2. goto apriltag")
+
+    def goal_cb(self, goal_msg):
+        global last_msg
 
         x = str(goal_msg.pose.position.x)
         y = str(goal_msg.pose.position.y)
         (r, p, yy) = tf.transformations.euler_from_quaternion([goal_msg.pose.orientation.x, goal_msg.pose.orientation.y, goal_msg.pose.orientation.z, goal_msg.pose.orientation.w])
-        
         theta = str(yy)
-    
         posn = np.asarray([x,y,theta], dtype=np.float64, order="C")
-        if self.stage==2:
+
+        if self.stage == 2:
             print(x,y,yy)
             if goal_msg.pose.position.y != last_msg:
                 last_msg = goal_msg.pose.position.y
+
                 rospy.loginfo("Go to the goal: [%s , %s].", x, y)
-                self.go_to_relative( posn)
+                self.go_to_relative(posn)
                 self.bot.base.stop()
                 rospy.loginfo("Achieve the goal.")
-        
 
-    def go_to_relative(self,posn):
-    
+    def go_to_relative(self, posn):
         self.bot.base.go_to_relative(
-        posn, use_map=False, close_loop=True, smooth=True
+            posn, use_map=False, close_loop=True, smooth=True
         )
-        # rospy.set_param('/goal_point/stage', stage+1)
-
 
 if __name__ == "__main__":
     rospy.init_node('navigation')
     teleop = rospy.get_param("teleop", False) 
-    ff=Robott()
-    # gg=Grasper()
+    locobot_controller = LocobotController()
     rospy.loginfo("Wait for inputting goal points.")
     rospy.spin()
